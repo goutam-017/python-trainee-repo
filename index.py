6df9e25aaa153f3e2401328ba62b0c6fcd854536
goutam-017 <senapatigoutam74@gmail.com>
2026-04-21 10:44:12 +0530

    changes in index.py  file

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
-name=input('Enter a Name:')
+# name=input('Enter a Name:')
 
-print(name)
+# print(name)
+
+
+
+def hello(first_name,last_name):
+    print('First Name: ',first_name)
+    print('Last Name: ',last_name)
+
+hello(last_name='Senapati',first_name='Goutam')
