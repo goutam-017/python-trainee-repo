648edd3ed6da35e509587757ff5c9b2e82b3ffd0
goutam-017 <senapatigoutam74@gmail.com>
2026-04-21 12:20:02 +0530

    adding some new code

--- oop.py
+++ oop.py
@@ -4,3 +4,16 @@ class Bike:
 
 bike1=Bike()
 print(f'Name: {bike1.name}, It have {bike1.gear} gear')
+
+
+class Employee:
+    employee_id = 0
+
+employee1 = Employee()
+employee2 = Employee()
+
+employee1.employeeID = 1001
+print(f"Employee ID: {employee1.employeeID}")
+
+employee2.employeeID = 1002
+print(f"Employee ID: {employee2.employeeID}")
