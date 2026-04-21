9d95946fc3178bfeed14fb2f9bc01ff7b0a1e3d7
goutam-017 <senapatigoutam74@gmail.com>
2026-04-21 15:31:57 +0530

    adding some new code

--- oop.py
+++ oop.py
@@ -17,3 +17,40 @@ print(f"Employee ID: {employee1.employeeID}")
 
 employee2.employeeID = 1002
 print(f"Employee ID: {employee2.employeeID}")
+
+class Room:
+    length=0.0
+    breadth=0.0
+
+    def calculate_area(self):
+        total=self.length*self.breadth
+        print(f'Area of room is= {total}')
+
+room=Room()
+room.length=float(input("Enter Length= "))
+room.breadth=float(input("Enter breadth= "))
+room.calculate_area()
+
+class NewBike:
+    def __init__(self,name):
+        self.name=name
+    def dis(self):
+        print(self.name)
+
+new_bike=NewBike('Mountain Bike')
+new_bike.dis()
+
+# inheritance
+class Animal:
+    name=''
+    def eat(self):
+        print("I can eat.")
+
+class Dog(Animal):
+    def display(self):
+        print(f'My name is {self.name}.')
+
+dog=Dog()
+dog.name='Sheru'
+dog.display()
+dog.eat()
