3e9d2bf96cf5a890cebbf57f0a10428b1fe90e10
goutam-017 <senapatigoutam74@gmail.com>
2026-04-22 08:33:23 +0530

    Adding some extra feature at division clculations

--- simplecalculator.py
+++ simplecalculator.py
@@ -4,23 +4,29 @@ def simplecalculator(a,s,b):
         if s in symbol_list:
             if(s=='+'):
                 sum=a+b
-                return f'Addition of two number is:{sum}'
+                return f'Addition of two number is: {sum}'
             elif(s=='-'):
                 sub=a-b
-                return f'Substraction of two number is:{sub}'
+                return f'Substraction of two number is: {sub}'
             elif(s=='*'):
                 mul=a*b
-                return f'Multiplication of two number is:{mul}'
+                return f'Multiplication of two number is: {mul}'
             elif(s=='/'):
-                div=a//b
-                return f'Division of two number is:{div}'
+                if(type(a)==float and type(b)==float):
+                    div=a/b
+                elif(type(a)==float and type(b)==int):
+                    div=a/b
+                elif(type(a)==int and type(b)==float):
+                    div=a/b
+                else:
+                    div=a//b
+                return f'Division of two number is: {div}'
         else:
             return 'Please enter a valid operation symbol..'
     except ZeroDivisionError:
         return 'Error: Denominator cannot be 0.'
 
-a=int(input('Enter the first number: '))
+a=eval(input('Enter the first number: '))
+b=eval(input('Enter the second number: '))
 s=input('Enter which operation you want (+, -, *, /) : ')
-b=int(input('Enter the second number: '))
-
 print(simplecalculator(a,s,b))
