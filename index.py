54c5da10513d0b81415becbd33af95e0c0cf7e77
goutam-017 <senapatigoutam74@gmail.com>
2026-04-23 10:47:41 +0530

    added some new codes

--- index.py
+++ index.py
@@ -21,3 +21,30 @@ def hello(a):
     print(a)
 hello(a)    
 print(a)
+
+
+functions = []
+
+for i in range(4):
+    functions.append(lambda j=i: j)
+print(type(functions))
+print(type(functions[0]))
+print(functions[0]())
+for f in functions:
+    print(f())
+
+
+a='goutam'
+b='goutam'
+a=[1,2,3]
+b=[1,2,3]
+print(id(a))
+print(id(b))
+
+print(a == b)
+print(a is b)
+
+nums = [x*x for x in range(5)]
+gen = (x*x for x in range(5))
+print(nums)
+print(gen)
