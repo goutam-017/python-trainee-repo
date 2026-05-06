cef04a4a652856f351454fbdd82dec1234c4ef95
goutam-017 <senapatigoutam74@gmail.com>
2026-05-06 13:04:46 +0530

    add some code change

--- repy.py
+++ repy.py
@@ -9,3 +9,17 @@ print(o)
 string = "Everything is <replaced> if it's in <tags>."
 s=re.sub("<.*?>",'Elephants',string)
 print(s)
+
+with open("raw/chunk_0001.txt",'r') as f:
+    text=f.read()
+
+pattern_date=[r"\d{1,2}\s*[/-]\s*\d{1,2}\s*[/-]\s*\d{4}",
+              r"\d{4}\s*[/-]\s*\d{1,2}\s*[/-]\s*\d{1,2}"]
+
+dates=[]
+for p in pattern_date:
+    d=re.findall(p,text)
+    dates.extend(d)
+
+for d in dates:
+    print(d)
